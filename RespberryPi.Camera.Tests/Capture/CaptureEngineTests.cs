c970944101c4aff8f22e52af8e11c0bf1b15dabe
Ben Schmidt <dr.ben.schmidt@gmail.com>
2018-03-17 20:56:33 -0500

    Inititial Checkin.

---
+++ RespberryPi.Camera.Tests/Capture/CaptureEngineTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaspberryPi.Camera.Capture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RespberryPi.Camera.Tests.Capture
+{
+    [TestClass]
+    public class CaptureEngineTests
+    {
+        [TestMethod]
+        public void CanGetCaptureSources()
+        {
+            var engine = new CaptureEngine();
+
+            engine.GetSources().Wait();
+        }
+    }
+}
